ef984006fd0f76f0088f1100507a6f70464cc364
Ricardo Gauto <ricardogauto11@gmail.com>
2019-03-05 02:01:16 -0300

    Settings and auth routes added

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express')
 const app = express();
 
+// Settings
+app.set('views', __dirname + '/views')
+app.set('view engine', 'pug')
+
 app.get('/', (req, res) => {
-    res.end('Hello world!')
+    res.render('index.pug')
 })
+
+app.get('/login', (req, res) => {
+    res.render('auth/login.pug')
+})
+
+app.get('/reset-password', (req, res) => {
+    res.render('auth/reset-password.pug')
+})
+
+app.get('/register', (req, res) => {
+    res.render('auth/register.pug')
+})
+
+app.get('*', (req, res) => {
+    res.end('404 not found')
+})
+
 app.listen(3000, () => {
     console.log('listening\nPress Ctrl+C to quit')
 })
